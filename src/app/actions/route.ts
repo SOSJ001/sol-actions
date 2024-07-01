4fb4388e0a2ca8655b61ff447ac92cd05cb69038
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-07-01 00:21:26 +0000

    Update route.ts

--- src/app/actions/route.ts
+++ src/app/actions/route.ts
@@ -27,7 +27,7 @@ export const GET = async (req: Request) => {
             actions: [
                 {
                     label: "Send me 0.002 sol",
-                    href: "http://localhost:3000/actions",
+                    href: "https://sol-actions-j0q4dzrlu-sosj001s-projects.vercel.app/actions",
                 }
             ]
         }
